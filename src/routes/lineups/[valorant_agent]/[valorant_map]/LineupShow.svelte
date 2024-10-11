a4d2a83391d349b730fce346ce6940e0b7dfa6d3
WingedSeal <firm09719@gmail.com>
2024-10-12 00:51:59 +0700

    feat: update layout

--- src/routes/lineups/[valorant_agent]/[valorant_map]/LineupShow.svelte
+++ src/routes/lineups/[valorant_agent]/[valorant_map]/LineupShow.svelte
@@ -6,7 +6,11 @@
 	<div class="grid grid-cols-3 grid-rows-3 aspect-video gap-6 -translate-x-[3.3333%] m-auto">
 		<div class="col-span-2 row-span-2 relative">
 			<canvas class="bg-black aspect-video w-[90%] h-[90%] bottom-0 right-0 absolute" />
-			<h2 class="text-7xl font-bold text-primary absolute left-[9.5%] top-[-3%]">MAP AGENT</h2>
+			<h2
+				class="text-7xl font-bold text-primary absolute right-[90.5%] bottom-[87%] translate-x-full w-full"
+			>
+				MAP AGENT
+			</h2>
 		</div>
 		<div>
 			<canvas class="bg-black aspect-video w-full h-full" />
@@ -16,9 +20,12 @@
 		</div>
 		<div class="relative">
 			<div
-				class="grid grid-cols-1 grid-rows-3 w-[80%] absolute right-0 h-full
+				class="grid grid-cols-1 grid-rows-3 w-[80%] absolute right-0 h-full justify-center
                 uppercase text-3xl text-primary font-bold content-center tracking-widest"
 			>
+				<!-- <div class="flex"><h2 class="my-auto">normal throw</h2></div>
+                <div class="flex"><h2 class="my-auto">grade: S</h2></div>
+                <div class="flex"><h2 class="my-auto">description</h2></div> -->
 				<h2>normal throw</h2>
 				<h2>grade: S</h2>
 				<h2>description</h2>
