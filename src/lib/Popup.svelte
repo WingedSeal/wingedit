0d829bbaa3e93b746f5c9d28699a64a67d6ddbf9
WingedSeal <firm09719@gmail.com>
2024-10-11 19:01:48 +0700

    feat: add lineup show

--- src/lib/Popup.svelte
+++ src/lib/Popup.svelte
@@ -2,8 +2,8 @@
 	import '@fortawesome/fontawesome-free/css/all.min.css';
 	import './Popup.css';
 	export let title: string;
-	export let size_x: number = 50;
-	export let size_y: number = 50;
+	export let size_x: number = 80;
+	export let size_y: number = 80;
 	export let is_hidden = false;
 </script>
 
@@ -18,7 +18,7 @@
 		class="fixed bg-slate-700 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-xl z-30 appear-animation"
 		style="width: {size_x}vw; height: {size_y}vh;"
 	>
-		<div class="bg-slate-800 w-full h-[10%] rounded-xl text-primary p-1 flex">
+		<div class="bg-slate-800 w-full h-[7%] rounded-xl text-primary p-1 flex">
 			<h1 class="my-auto ml-6 text-xl">{title}</h1>
 			<button
 				class="my-auto ml-auto mr-3 text-xl cursor-pointer"
@@ -29,6 +29,6 @@
 				<i class="fa-regular fa-circle-xmark" />
 			</button>
 		</div>
-		<div class="w-full h-full p-2"><slot /></div>
+		<div class="w-full h-[93%] p-2"><slot /></div>
 	</div>
 {/if}
--- src/routes/lineups/[valorant_agent]/[valorant_map]/LineupShow.svelte
+++ src/routes/lineups/[valorant_agent]/[valorant_map]/LineupShow.svelte
@@ -1,3 +1,74 @@
 <script lang="ts"></script>
 
-<p class="text-primary">LINEUP_SHOW_TEST</p>
+<div class="w-full h-full lineup-grid grid gap-x-6 gap-y-6 py-4 px-36">
+	<div class="how-lineup relative">
+		<div class="w-full h-[90%] absolute bottom-0">
+			<div class="w-full h-full bg-red-400 absolute flex">
+				<p class="m-auto">How to lineup</p>
+			</div>
+		</div>
+		<div class="absolute text-7xl -top-3 -left-1 font-bold text-primary">MAP AGENT_NAME</div>
+	</div>
+	<div class="throw-gif relative">
+		<div class="w-full h-full bg-purple-300 absolute flex">
+			<p class="m-auto">THROW GIF</p>
+		</div>
+	</div>
+	<div class="land-spot relative">
+		<div class="w-full h-full bg-green-300 absolute flex">
+			<p class="m-auto">LAND SPOT</p>
+		</div>
+	</div>
+	<div class="lineup-text relative">
+		<div
+			class="w-full h-full absolute grid grid-cols-1 grid-rows-3 flex-col
+            uppercase text-4xl text-primary font-bold content-center tracking-widest"
+		>
+			<h2>normal throw</h2>
+			<h2>grade: S</h2>
+			<h2>description</h2>
+		</div>
+	</div>
+	<div class="first-person relative">
+		<div class="w-full h-full bg-blue-300 absolute flex">
+			<p class="m-auto">FIRST PERSON</p>
+		</div>
+	</div>
+	<div class="third-person relative">
+		<div class="w-full h-full bg-fuchsia-300 absolute flex">
+			<p class="m-auto">THIRD PERSON</p>
+		</div>
+	</div>
+</div>
+
+<style lang="css">
+	.lineup-grid {
+		grid-template-areas:
+			'how-lineup how-lineup throw-gif'
+			'how-lineup how-lineup land-spot'
+			'lineup-text first-person third-person';
+	}
+
+	.how-lineup {
+		grid-area: how-lineup;
+	}
+
+	.throw-gif {
+		grid-area: throw-gif;
+	}
+	.land-spot {
+		grid-area: land-spot;
+	}
+
+	.lineup-text {
+		grid-area: lineup-text;
+	}
+
+	.first-person {
+		grid-area: first-person;
+	}
+
+	.third-person {
+		grid-area: third-person;
+	}
+</style>
