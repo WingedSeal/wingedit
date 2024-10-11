5122f2e91f16a63209f3569f265c6c022385426e
WingedSeal <firm09719@gmail.com>
2024-10-12 00:29:19 +0700

    feat: increase gap

--- src/routes/lineups/[valorant_agent]/[valorant_map]/LineupShow.svelte
+++ src/routes/lineups/[valorant_agent]/[valorant_map]/LineupShow.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <ElementFit>
-	<div class="grid grid-cols-3 grid-rows-3 aspect-video gap-3 -translate-x-[3.3333%] m-auto">
+	<div class="grid grid-cols-3 grid-rows-3 aspect-video gap-6 -translate-x-[3.3333%] m-auto">
 		<div class="col-span-2 row-span-2 relative">
 			<canvas class="bg-black aspect-video w-[90%] h-[90%] bottom-0 right-0 absolute" />
-			<h2 class="text-7xl font-bold text-primary absolute left-[9%] top-[-2%]">MAP AGENT</h2>
+			<h2 class="text-7xl font-bold text-primary absolute left-[9.5%] top-[-3%]">MAP AGENT</h2>
 		</div>
 		<div>
 			<canvas class="bg-black aspect-video w-full h-full" />
@@ -17,7 +17,7 @@
 		<div class="relative">
 			<div
 				class="grid grid-cols-1 grid-rows-3 w-[80%] absolute right-0 h-full
-                uppercase text-3xl text-primary font-bold content-center tracking-wider"
+                uppercase text-3xl text-primary font-bold content-center tracking-widest"
 			>
 				<h2>normal throw</h2>
 				<h2>grade: S</h2>
