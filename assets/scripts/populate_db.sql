a3b11edb0f17c4615328942916c8e47c6af65cb0
WingedSeal <firm09719@gmail.com>
2024-10-14 17:55:54 +0700

    feat: add more agents

--- assets/scripts/populate_db.sql
+++ assets/scripts/populate_db.sql
@@ -1,15 +1,36 @@
 INSERT INTO
     "Agents" ("ID", "Name", "RoleID")
 VALUES
-    (1, 'Brimstone', 1),
-    (2, 'Viper', 1),
-    (3, 'Omen', 1),
-    (4, 'Killjoy', 4);
+    (1, "Brimstone", 1),
+    (2, "Viper", 1),
+    (3, "Omen", 1),
+    (4, "Killjoy", 4),
+    (5, "Cypher", 4),
+    (6, "Sova", 3),
+    (7, "Sage", 4),
+    (9, "Phoenix", 2),
+    (10, "Jett", 2),
+    (11, "Reyna", 2),
+    (12, "Raze", 2),
+    (13, "Breach", 3),
+    (14, "Skye", 3),
+    (15, "Yoru", 2),
+    (16, "Astra", 1),
+    (17, "Kayo", 3),
+    (18, "Chamber", 4),
+    (19, "Neon", 2),
+    (20, "Fade", 3),
+    (21, "Harbor", 1),
+    (22, "Gekko", 3),
+    (23, "Deadlock", 4),
+    (24, "Iso", 2),
+    (25, "Clove", 1),
+    (26, "Vsye", 4);
 
 INSERT INTO
     "AgentRoles" ("RoleID", "Name")
 VALUES
-    (1, 'Controller'),
-    (2, 'Duelist'),
-    (3, 'Initiator'),
-    (4, 'Sentinel');
+    (1, "Controller"),
+    (2, "Duelist"),
+    (3, "Initiator"),
+    (4, "Sentinel");
