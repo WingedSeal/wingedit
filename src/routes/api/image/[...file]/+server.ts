c1a8465efff3d1b3be923122af59dd6c22cbb7d4
WingedSeal <firm09719@gmail.com>
2024-10-15 14:01:17 +0700

    feat: 404 if image not found

--- src/routes/api/image/[...file]/+server.ts
+++ src/routes/api/image/[...file]/+server.ts
@@ -1,7 +1,13 @@
 import { IMAGES_PATH } from '$env/static/private';
+import { error } from '@sveltejs/kit';
 import fs from 'fs';
 import path from 'path';
 
 export const GET = async ({ params }) => {
-	return new Response(fs.readFileSync(path.join(IMAGES_PATH, params.file)));
+	const image_path = path.join(IMAGES_PATH, params.file);
+	if (fs.existsSync(image_path)) {
+		return new Response(fs.readFileSync(image_path));
+	} else {
+		error(404, 'Image not found');
+	}
 };
