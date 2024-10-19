412ac2c159a086bc385c62096d66d87e5f862a8a
WingedSeal <firm09719@gmail.com>
2024-10-19 20:29:22 +0700

    feat: update MapPositions

--- assets/scripts/create_tables.sql
+++ assets/scripts/create_tables.sql
@@ -34,9 +34,11 @@ CREATE TABLE
 CREATE TABLE
     IF NOT EXISTS "MapPositions" (
         "ID" SMALLINT NOT NULL,
-        "Callout" VARCHAR(32) NOT NULL,
         "MapID" TINYINT NOT NULL,
-        PRIMARY KEY ("ID"),
+        "Callout" VARCHAR(32) NOT NULL,
+        "PosX" DECIMAL(5, 2) NOT NULL,
+        "PosY" DECIMAL(5, 2) NOT NULL,
+        PRIMARY KEY ("ID", "MapID"),
         CONSTRAINT "FK_Maps_MapID" FOREIGN KEY ("MapID") REFERENCES "Maps" ("ID") ON DELETE RESTRICT ON UPDATE RESTRICT,
         CONSTRAINT "UQ_Callout_MapID" UNIQUE ("Callout", "MapID")
     );
