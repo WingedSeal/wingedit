4684aa59902ab4f2ff27fa28c72e9f2977e8de57
WingedSeal <firm09719@gmail.com>
2024-10-26 19:13:45 +0700

    feat: use $derived.by instead of $effect

--- src/lib/components/LineupShowOverlay.svelte
+++ src/lib/components/LineupShowOverlay.svelte
@@ -23,59 +23,42 @@
 	const subSizePercent = 2 * SIZE_MULTIPLIER;
 	let clientHeight = $state<number>();
 	let clientWidth = $state<number>();
-	let main1 = $state<[number, number]>();
-	let main2 = $state<[number, number]>();
-	let sub1 = $state<[number, number]>();
-	let sub2 = $state<[number, number]>();
-	let hasSub = $state(false);
-	$effect(() => {
-		clientHeight;
-		clientWidth;
-		DrawOverMainX;
-		DrawOverMainY;
-		DrawOverSub1X;
-		DrawOverSub1Y;
-		DrawOverSub2X;
-		DrawOverSub2Y;
-		untrack(() => {
-			if (
-				DrawOverSub1X === null ||
-				DrawOverSub1Y === null ||
-				DrawOverSub2X === null ||
-				DrawOverSub2Y === null
-			) {
-				hasSub = false;
-			} else {
-				hasSub = true;
+	const { main1, main2, sub1, sub2 } = $derived.by(() => {
+		if (
+			DrawOverSub1X === null ||
+			DrawOverSub1Y === null ||
+			DrawOverSub2X === null ||
+			DrawOverSub2Y === null
+		)
+			return { main1: undefined, main2: undefined, sub1: undefined, sub2: undefined };
 
-				const main = [
-					(DrawOverMainX * clientWidth!) / 100,
-					((100 - DrawOverMainY) * clientHeight!) / 100
-				];
-				sub1 = [
-					(DrawOverSub1X! * clientWidth!) / 100,
-					((100 - DrawOverSub1Y!) * clientHeight!) / 100
-				];
-				sub2 = [
-					(DrawOverSub2X! * clientWidth!) / 100,
-					((100 - DrawOverSub2Y!) * clientHeight!) / 100
-				];
-				const dx1 = sub1[0] - main[0];
-				const dy1 = sub1[1] - main[1];
-				const dx2 = sub2[0] - main[0];
-				const dy2 = sub2[1] - main[1];
-				const length1 = Math.sqrt(dx1 * dx1 + dy1 * dy1);
-				const length2 = Math.sqrt(dx2 * dx2 + dy2 * dy2);
-				const mainRadius = (mainSizePercent * clientHeight!) / 200;
-				const subRadius = (subSizePercent * clientHeight!) / 200;
-				sub1[0] -= (dx1 * subRadius) / length1;
-				sub1[1] -= (dy1 * subRadius) / length1;
-				sub2[0] -= (dx2 * subRadius) / length2;
-				sub2[1] -= (dy2 * subRadius) / length2;
-				main1 = [main[0] + (dx1 * mainRadius) / length1, main[1] + (dy1 * mainRadius) / length1];
-				main2 = [main[0] + (dx2 * mainRadius) / length2, main[1] + (dy2 * mainRadius) / length2];
-			}
-		});
+		const main = [
+			(DrawOverMainX * clientWidth!) / 100,
+			((100 - DrawOverMainY) * clientHeight!) / 100
+		];
+		let sub1 = [
+			(DrawOverSub1X! * clientWidth!) / 100,
+			((100 - DrawOverSub1Y!) * clientHeight!) / 100
+		];
+		let sub2 = [
+			(DrawOverSub2X! * clientWidth!) / 100,
+			((100 - DrawOverSub2Y!) * clientHeight!) / 100
+		];
+		const dx1 = sub1[0] - main[0];
+		const dy1 = sub1[1] - main[1];
+		const dx2 = sub2[0] - main[0];
+		const dy2 = sub2[1] - main[1];
+		const length1 = Math.sqrt(dx1 * dx1 + dy1 * dy1);
+		const length2 = Math.sqrt(dx2 * dx2 + dy2 * dy2);
+		const mainRadius = (mainSizePercent * clientHeight!) / 200;
+		const subRadius = (subSizePercent * clientHeight!) / 200;
+		sub1[0] -= (dx1 * subRadius) / length1;
+		sub1[1] -= (dy1 * subRadius) / length1;
+		sub2[0] -= (dx2 * subRadius) / length2;
+		sub2[1] -= (dy2 * subRadius) / length2;
+		const main1 = [main[0] + (dx1 * mainRadius) / length1, main[1] + (dy1 * mainRadius) / length1];
+		const main2 = [main[0] + (dx2 * mainRadius) / length2, main[1] + (dy2 * mainRadius) / length2];
+		return { main1, main2, sub1, sub2 };
 	});
 </script>
 
@@ -85,7 +68,7 @@
 					rounded-[50%] border-solid border-red-600"
 		style={`height: ${mainSizePercent}%; left: ${DrawOverMainX}%; bottom: ${DrawOverMainY}%; border-width: ${clientHeight * 0.005 * SIZE_MULTIPLIER}px;`}
 	></div>
-	{#if hasSub}
+	{#if sub1 && sub2}
 		<div
 			class="aspect-square absolute bg-transparent -translate-x-1/2 translate-y-1/2
 					rounded-[50%] border-solid border-red-300"
