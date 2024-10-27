4d2e4bd1fdab447515f7eb93e093b90afcbfbd61
WingedSeal <firm09719@gmail.com>
2024-10-27 21:45:34 +0700

    refactor: improve require_all_images

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -68,97 +68,52 @@ const decimalSchema = z
 	.default(null as unknown as number);
 
 export const getLineupSchema = (_imageSchema: ZodType, _gifSchema: ZodType) => {
-	if (PUBLIC_REQUIRE_ALL_IMAGES === 'true') {
-		return z
-			.object({
-				agent: z.number().int().min(1, 'Please select an agent.'),
-				map: z.number().int().min(1, 'Please select a map.'),
-				ability: z.number().int().min(1, 'Please select an agent ability.'),
-				throwLineup: _imageSchema,
-				throwGif: _gifSchema,
-				landSpot: _imageSchema,
-				throwSpotFirstPerson: _imageSchema,
-				throwSpotThirdPerson: _imageSchema,
-				grade: z.number().int().min(1, 'Please select a grade.'),
-				throwType: z.number().int().min(1, 'Please select a throw type.'),
-				timeToLand: z
-					.number({ message: 'Expected a number.' })
-					.positive()
-					.max(300)
-					.refine(
-						(x) => x * 100 - Math.trunc(x * 100) < Number.EPSILON,
-						'Expected up to 2 decimal points.'
-					)
-					.default('' as unknown as number),
-				description: z.string().trim(),
-				mainX: decimalSchema.default(50),
-				mainY: decimalSchema.default(50),
-				sub1X: decimalSchema.nullable(),
-				sub1Y: decimalSchema.nullable(),
-				sub2X: decimalSchema.nullable(),
-				sub2Y: decimalSchema.nullable(),
-				from: z.number().int().min(1, 'Please select map position.'),
-				to: z.number().int().min(1, 'Please select map position.'),
-				fromX: decimalSchema,
-				fromY: decimalSchema,
-				toX: decimalSchema,
-				toY: decimalSchema
-			})
-			.refine((data) => {
-				if ([data.sub1X, data.sub1Y, data.sub2X, data.sub2Y].every((x) => x === null)) {
-					return true;
-				}
-				if ([data.sub1X, data.sub1Y, data.sub2X, data.sub2Y].every((x) => x !== null)) {
-					return true;
-				}
-				return false;
-			}, 'sub1X, sub1Y, sub2X, sub2Y must either be all null or all non-null');
-	} else {
-		return z
-			.object({
-				agent: z.number().int().min(1, 'Please select an agent.'),
-				map: z.number().int().min(1, 'Please select a map.'),
-				ability: z.number().int().min(1, 'Please select an agent ability.'),
-				throwLineup: _imageSchema,
-				throwGif: _gifSchema.nullable(),
-				landSpot: _imageSchema.nullable(),
-				throwSpotFirstPerson: _imageSchema.nullable(),
-				throwSpotThirdPerson: _imageSchema.nullable(),
-				grade: z.number().int().min(1, 'Please select a grade.'),
-				throwType: z.number().int().min(1, 'Please select a throw type.'),
-				timeToLand: z
-					.number({ message: 'Expected a number.' })
-					.positive()
-					.max(300)
-					.refine(
-						(x) => x * 100 - Math.trunc(x * 100) < Number.EPSILON,
-						'Expected up to 2 decimal points.'
-					)
-					.default('' as unknown as number),
-				description: z.string().trim(),
-				mainX: decimalSchema.default(50),
-				mainY: decimalSchema.default(50),
-				sub1X: decimalSchema.nullable(),
-				sub1Y: decimalSchema.nullable(),
-				sub2X: decimalSchema.nullable(),
-				sub2Y: decimalSchema.nullable(),
-				from: z.number().int().min(1, 'Please select map position.'),
-				to: z.number().int().min(1, 'Please select map position.'),
-				fromX: decimalSchema,
-				fromY: decimalSchema,
-				toX: decimalSchema,
-				toY: decimalSchema
-			})
-			.refine((data) => {
-				if ([data.sub1X, data.sub1Y, data.sub2X, data.sub2Y].every((x) => x === null)) {
-					return true;
-				}
-				if ([data.sub1X, data.sub1Y, data.sub2X, data.sub2Y].every((x) => x !== null)) {
-					return true;
-				}
-				return false;
-			}, 'sub1X, sub1Y, sub2X, sub2Y must either be all null or all non-null');
-	}
+	const require_all_images = PUBLIC_REQUIRE_ALL_IMAGES === 'true';
+	const nullableImageSchema = require_all_images ? _imageSchema.nullable() : _imageSchema;
+	return z
+		.object({
+			agent: z.number().int().min(1, 'Please select an agent.'),
+			map: z.number().int().min(1, 'Please select a map.'),
+			ability: z.number().int().min(1, 'Please select an agent ability.'),
+			throwLineup: _imageSchema,
+			throwGif: require_all_images ? _gifSchema : _gifSchema.nullable(),
+			landSpot: nullableImageSchema,
+			throwSpotFirstPerson: nullableImageSchema,
+			throwSpotThirdPerson: nullableImageSchema,
+			grade: z.number().int().min(1, 'Please select a grade.'),
+			throwType: z.number().int().min(1, 'Please select a throw type.'),
+			timeToLand: z
+				.number({ message: 'Expected a number.' })
+				.positive()
+				.max(300)
+				.refine(
+					(x) => x * 100 - Math.trunc(x * 100) < Number.EPSILON,
+					'Expected up to 2 decimal points.'
+				)
+				.default('' as unknown as number),
+			description: z.string().trim(),
+			mainX: decimalSchema.default(50),
+			mainY: decimalSchema.default(50),
+			sub1X: decimalSchema.nullable(),
+			sub1Y: decimalSchema.nullable(),
+			sub2X: decimalSchema.nullable(),
+			sub2Y: decimalSchema.nullable(),
+			from: z.number().int().min(1, 'Please select map position.'),
+			to: z.number().int().min(1, 'Please select map position.'),
+			fromX: decimalSchema,
+			fromY: decimalSchema,
+			toX: decimalSchema,
+			toY: decimalSchema
+		})
+		.refine((data) => {
+			if ([data.sub1X, data.sub1Y, data.sub2X, data.sub2Y].every((x) => x === null)) {
+				return true;
+			}
+			if ([data.sub1X, data.sub1Y, data.sub2X, data.sub2Y].every((x) => x !== null)) {
+				return true;
+			}
+			return false;
+		}, 'sub1X, sub1Y, sub2X, sub2Y must either be all null or all non-null');
 };
 
 export const mapSchema = z.object({
