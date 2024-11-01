da42a45d7c62bc640dfe699cdca21df8185f93dc
Kamolpat Thananopavarn <kamolpatbcc@gmail.com>
2024-11-01 19:24:05 +0700

    feat: decorate signin page

--- src/routes/account/signin/+page.svelte
+++ src/routes/account/signin/+page.svelte
@@ -10,32 +10,62 @@
 	});
 </script>
 
-<h1>Sign in</h1>
-<form method="post" class="flex flex-col m-5" use:enhance>
-	<label for="username">Username</label>
-	<input name="username" id="username" bind:value={$form.username} />
-	{#if $errors.username}
-		<small>{$errors.username[0]}</small>
-	{/if}
+<!-- all container to make it be at the center-->
+<div class="flex items-center justify-center min-h-screen bg-gray-600">
+	<!-- smaller container-->
+	<div class="bg-white p-8 rounded-lg max-w-md w-full text-center">
+		<h1 class="text-2xl font-semibold mb-6">Sign in</h1>
 
-	<label for="password">Password</label>
-	<input type="password" name="password" id="password" bind:value={$form.password} />
-	{#if $errors.password}
-		<small>{$errors.password[0]}</small>
-	{/if}
-	<button>Continue</button>
-</form>
+		<form method="post" class="flex flex-col" use:enhance>
+			<label for="username" class="text-left font-medium mb-1">Username</label>
+			<input
+				name="username"
+				id="username"
+				bind:value={$form.username}
+				class="border border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+			/>
+			{#if $errors.username}
+				<small class="text-red-500 text-left mb-2">{$errors.username[0]}</small>
+			{/if}
 
-<form method="post" use:enhance action="/account/signout">
-	<button>Sign out</button>
-</form>
-{#if $message}
-	{$message}
-{/if}
-<br />
-{#if $errors._errors}
-	{$errors._errors}
-{/if}
-<br />
+			<label for="password" class="text-left font-medium mb-1">Password</label>
+			<input
+				type="password"
+				name="password"
+				id="password"
+				bind:value={$form.password}
+				class="border border-gray-300 rounded-md px-4 py-2 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+			/>
+			{#if $errors.password}
+				<small class="text-red-500 text-left mb-2">{$errors.password[0]}</small>
+			{/if}
 
-<a href={'/account/signup' + $page.url.search}>No Account?</a>
+			<button
+				class="bg-blue-500 text-white rounded-md py-2 mt-2 hover:bg-blue-600 transition-colors"
+				>Continue</button
+			>
+		</form>
+
+		<form method="post" use:enhance action="/account/signout" class="mt-4">
+			<button
+				class="bg-gray-300 text-gray-700 rounded-md py-2 w-full hover:bg-gray-400 transition-colors"
+				>Sign out</button
+			>
+		</form>
+
+		{#if $message}
+			<div class="text-green-500 mt-4">{$message}</div>
+		{/if}
+
+		{#if $errors._errors}
+			<div class="text-red-500 mt-4">{$errors._errors}</div>
+		{/if}
+		<h1 class="text-blue-500 mt-4 inline-block">Don't have account?</h1>
+		<a
+			href={'/account/signup' + $page.url.search}
+			class="text-blue-500 mt-4 inline-block hover:underline"
+		>
+			Sign up
+		</a>
+	</div>
+</div>
