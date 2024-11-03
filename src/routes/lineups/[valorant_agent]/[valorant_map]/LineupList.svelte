66032433a36439e6fe463efc80b470ac7cb18da3
WingedSeal <firm09719@gmail.com>
2024-11-03 18:12:57 +0700

    feat: implement functioning? sort table for lineupList

--- src/routes/lineups/[valorant_agent]/[valorant_map]/LineupList.svelte
+++ src/routes/lineups/[valorant_agent]/[valorant_map]/LineupList.svelte
@@ -13,41 +13,107 @@
 
 	const getDetails = (lineup: Lineup) => {
 		return {
-			ID: lineup.ID,
+			lineup: lineup,
 			abilityNameID: abilities[lineup.AbilityID].NameID,
 			ability: abilities[lineup.AbilityID].Name,
 			grade: gameInfo.grades[lineup.GradeID].Name,
 			side: gameInfo.sides[lineup.SideID].Name,
 			from: gameInfo.mapPositions[lineup.MapID][lineup.FromMapPositionID].Callout,
 			to: gameInfo.mapPositions[lineup.MapID][lineup.ToMapPositionID].Callout,
-			timeToLand: lineup.TimeToLand,
 			difficulty: gameInfo.grades[lineup.Difficulty].Name
 		};
 	};
 
-	const details = $derived(
+	type Detail = ReturnType<typeof getDetails>;
+
+	let details = $state(
 		lineups.map((lineup) => {
 			return getDetails(lineup);
 		})
 	);
+
+	const compareStr = (a: string, b: string) => (a > b ? -1 : 1);
+	const sortFunc = (a: Detail, b: Detail): number => {
+		switch (sortedBy) {
+			case 'ID':
+				return a.lineup.ID! - b.lineup.ID!;
+			case 'Ability':
+				return compareStr(a.ability, b.ability);
+			case 'Grade':
+				return b.lineup.GradeID - a.lineup.GradeID;
+			case 'Side':
+				return compareStr(a.side, b.side);
+			case 'From':
+				return compareStr(a.from, b.from);
+			case 'To':
+				return compareStr(a.to, b.to);
+			case 'Time to land':
+				return a.lineup.TimeToLand - b.lineup.TimeToLand;
+			case 'Difficulty':
+				return b.lineup.Difficulty - a.lineup.Difficulty;
+		}
+		throw new Error('Unexpected sortedBy');
+	};
+
+	let sortedBy = $state('ID');
+	let isSortAscending = $state(true);
+	const reSort = () => {
+		details.sort((a, b) => {
+			return (isSortAscending ? 1 : -1) * sortFunc(a, b);
+		});
+	};
+	let getSortClass = (name: string) => {
+		if (name !== sortedBy) return 'sort';
+		if (isSortAscending) {
+			return 'sort-up';
+		} else {
+			return 'sort-down';
+		}
+	};
 </script>
+
+{#snippet tableHead(name: string)}
+	{@const sortClass = getSortClass(name)}
+	<th
+		>{name}
+		<button
+			aria-label={sortClass}
+			onclick={() => {
+				if (name !== sortedBy) {
+					isSortAscending = true;
+					sortedBy = name;
+					reSort();
+					return;
+				}
+				isSortAscending = !isSortAscending;
+				reSort();
+			}}
+		>
+			<i class="fa-solid fa-{sortClass}"></i>
+		</button>
+	</th>
+{/snippet}
 
 <table>
 	<thead>
 		<tr>
-			<th>Ability</th>
-			<th>Grade</th>
-			<th>Side</th>
-			<th>From</th>
-			<th>To</th>
-			<th>Time to land</th>
-			<th>Difficulty</th>
+			{@render tableHead('ID')}
+			{@render tableHead('Ability')}
+			{@render tableHead('Grade')}
+			{@render tableHead('Side')}
+			{@render tableHead('From')}
+			{@render tableHead('To')}
+			{@render tableHead('Time to land')}
+			{@render tableHead('Difficulty')}
 			<th></th>
 		</tr>
 	</thead>
 	<tbody>
-		{#each details as detail, i (detail.ID)}
+		{#each details as detail, i (detail.lineup.ID)}
 			<tr>
+				<td>
+					{detail.lineup.ID}
+				</td>
 				<td>
 					<img
 						class="inline h-4 -translate-y-1 bg-slate-800 rounded-md"
@@ -69,7 +135,7 @@
 					{detail.to}
 				</td>
 				<td>
-					{detail.timeToLand}s
+					{detail.lineup.TimeToLand}s
 				</td>
 				<td>
 					{detail.difficulty}
